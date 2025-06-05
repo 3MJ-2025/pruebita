bdc60401c2b3de53e3d27cf53709426b20d74691
Gonzalo <ggmartinez.utu@gmail.com>
2025-06-05 20:02:01 -0300

    Otro coso

--- index.php
+++ index.php
@@ -2,16 +2,4 @@
     require "funciones.php";
 
 
-    echo saludar();
-
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        
-    </body>
-    </html>
+   
